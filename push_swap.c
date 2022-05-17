58dd9aef4ab281ca5509a323ec24e66e54d8601c
Eloi Alonso quinonero <ealonso-@cbr6s3.42barcelona.com>
2022-05-17 18:40:17 +0200

    algorithm 3 & 5 ready

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,57 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 15:09:24 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/05/16 17:07:38 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/05/17 18:39:36 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	comprove(t_stacks *stack)
+{
+	int	i;
+
+	i = 1;
+	while (i < stack->alen)
+	{
+		if (stack->a[i - 1] > stack->a[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	mediumalg(t_stacks *stack)
+{
+	while (!comprove(stack))
+	{
+		if (stack->a[0] > stack->a[1])
+			sa(stack);
+		if (stack->a[0] > stack->a[stack->alen - 1])
+			rra(stack);
+		if (stack->a[0] < stack->a[1] && stack->a[0] < stack->a[stack->alen - 1]
+			&& !comprove(stack))
+			pb(stack);
+		if (comprove(stack))
+		{
+			pa(stack);
+			pa(stack);
+		}
+	}
+}
+
+void	threealg(t_stacks *stack)
+{
+	while (!comprove(stack))
+	{
+		if (stack->a[0] > stack->a[1])
+			sa(stack);
+		if (stack->a[1] > stack->a[2])
+			rra(stack);
+		if (stack->a[0] > stack->a[1] && stack->a[0] > stack->a[2])
+			ra(stack);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -26,5 +72,9 @@ int	main(int argc, char **argv)
 	stack.b = malloc(sizeof(int) * (stack.alen));
 	while (j < (argc - 1))
 		stack.a[j++] = ft_atoi(argv[i++]);
+	if (argc < 5)
+		threealg(&stack);
+	if (argc > 4 && argc < 7)
+		mediumalg(&stack); 
 	return (0);
 }
